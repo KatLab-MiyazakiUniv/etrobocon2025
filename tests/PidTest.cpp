219f638a57708c759c2fd55f32d4cf09b39fb2b0
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-05-18 02:09:04 +0900

    Squashed commit of the following:
    commit 2f42676113ce43672df68d9ab977dd53ae172577
    Author: miyahara046 <taka.rocket.nba.1104@gmail.com>
    Date:   Sun May 18 01:48:13 2025 +0900
    
        update:ゲインが0の時のテストの追加、誤字の修正
    
    commit bf07fe80278d40e0d281733197fab3a6ceb6c7ef
    Author: miyahara046 <taka.rocket.nba.1104@gmail.com>
    Date:   Sat May 17 20:43:27 2025 +0900
    
        update:gtestのアサーションをdauble型に変更
    
    commit 43a7bf54395a59a32f41f60296a53be679b17425
    Author: miyahara046 <taka.rocket.nba.1104@gmail.com>
    Date:   Sat May 17 20:40:43 2025 +0900
    
        update:ローパスフィルタの係数をメンバ変数に変更
    
    commit 7a3a5365afbe6dd3f7afdf4127f0da27c1b2f3f5
    Author: miyahara046 <taka.rocket.nba.1104@gmail.com>
    Date:   Sat May 17 15:14:16 2025 +0900
    
        add:Pid.cppのテストをするファイルの追加
    
    commit 84558db9f5444a2f387658f935f91bd53cc40d77
    Author: miyahara046 <taka.rocket.nba.1104@gmail.com>
    Date:   Sat May 17 15:12:15 2025 +0900
    
        add:Pidを計算するファイルの追加

--- tests/PidTest.cpp
+++ tests/PidTest.cpp
@@ -9,7 +9,7 @@
 namespace etrobocon2025_test {
 
   // PidGainの正のゲインがそのまま格納されるかをテスト
-  TEST(PidGainTest, pidGain)
+  TEST(PidGainTest, PidGainPlus)
   {
     double kp = 0.1;
     double ki = 0.03;
@@ -22,9 +22,23 @@ namespace etrobocon2025_test {
     EXPECT_DOUBLE_EQ(expected_ki, actualPidGain.ki);
     EXPECT_DOUBLE_EQ(expected_kd, actualPidGain.kd);
   }
+  // PidGainの0のゲインがそのまま格納されるかをテスト
+  TEST(PidGainTest, PidGainZero)
+  {
+    double kp = 0.0;
+    double ki = 0.0;
+    double kd = 0.0;
+    double expected_kp = 0.0;
+    double expected_ki = 0.0;
+    double expected_kd = 0.0;
+    PidGain actualPidGain(kp, ki, kd);
+    EXPECT_DOUBLE_EQ(expected_kp, actualPidGain.kp);
+    EXPECT_DOUBLE_EQ(expected_ki, actualPidGain.ki);
+    EXPECT_DOUBLE_EQ(expected_kd, actualPidGain.kd);
+  }
 
   // PidGainの負のゲインが0に補正されるかをテスト
-  TEST(PidGainTest, pidGainMinus)
+  TEST(PidGainTest, PidGainMinus)
   {
     double kp = -0.5;
     double ki = -0.2;
@@ -37,7 +51,7 @@ namespace etrobocon2025_test {
   }
 
   // 基本的なPID計算の結果が正しいかをテスト
-  TEST(PidTest, calculatePid)
+  TEST(PidTest, CalculatePid)
   {
     Pid actualPid(0.6, 0.02, 0.03, 70, 100.0, -100.0);
     double currentValue = 20;
@@ -59,10 +73,33 @@ namespace etrobocon2025_test {
     EXPECT_DOUBLE_EQ(expected, actualPid.calculatePid(currentValue));
   }
 
-  // 周期を変更した場合のPID計算結果をテスト
-  TEST(PidTest, calculatePidChangeDelta)
+  // ゲインが全て0の時のPID計算の結果が正しいかをテスト
+  TEST(PidTest, CalculatePidGainZero)
   {
-    constexpr double DELTA = 0.03;
+    Pid actualPid(0.0, 0.0, 0.0, 70, 100.0, -100.0);
+    double currentValue = 20;
+    /** 計算過程
+     * 1. 前回の誤差
+     * prevDeviation = 0
+     * 2. 現在の誤差
+     * currentDeviation = (70 - 20) = 50
+     * 3. 誤差の積分を計算
+     * integral = 0 + (50 + 0) * 0.01 / 2 = 0.25
+     * 4. 微分の処理を行う
+     * currentDerivative = (50 - 0) / 0.01 = 5000
+     * 微分項にローパスフィルタを適用
+     * prevDeviat filteredDerivative = 0.8 * 5000 + (1 - 0.8) * 0 = 4000
+     * 5. PID制御を計算
+     * expected = 0.0 * 50 + 0.0 * 0.25 + 0.0 * 4000 = 0.0
+     */
+    double expected = 0.0;
+    EXPECT_DOUBLE_EQ(expected, actualPid.calculatePid(currentValue));
+  }
+
+  // 周期を変更した場合のPID計算結果をテスト
+  TEST(PidTest, CalculatePidChangeDelta)
+  {
+    double DELTA = 0.03;
     Pid actualPid(0.6, 0.02, 0.03, 70, 100.0, -100.0);
     double currentValue = 55;
     /** 計算過程
@@ -84,7 +121,7 @@ namespace etrobocon2025_test {
   }
 
   // 周期に0を渡したとき、内部でデフォルトの0.01が使用されるかをテスト
-  TEST(PidTest, calculatePidChangeDeltaZero)
+  TEST(PidTest, CalculatePidChangeDeltaZero)
   {
     Pid actualPid(0.6, 0.02, 0.03, 70, 100.0, -100.0);
     double currentValue = 55;
@@ -94,20 +131,20 @@ namespace etrobocon2025_test {
      * 2. 現在の誤差
      * currentDeviation = (70 - 55) = 15
      * 3. 誤差の積分を計算
-     * integral = 0 + (15 + 0) * 0.01 / 2 = 0.072
+     * integral = 0 + (15 + 0) * 0.01 / 2 = 0.075
      * 4. 微分の処理を行う
      * currentDerivative = (15 - 0) / 0.01 = 1500
      * 微分項にローパスフィルタを適用
      * filteredDerivative = 0.8 * 1500 + (1 - 0.8) * 0 = 1200
      * 5. PID制御を計算
-     * expected = 0.6 * 15 + 0.02 * 0.072 + 0.03 * 1200 = 45.0015
+     * expected = 0.6 * 15 + 0.02 * 0.075 + 0.03 * 1200 = 45.0015
      */
     double expected = 45.0015;
     EXPECT_DOUBLE_EQ(expected, actualPid.calculatePid(currentValue, 0.0));
   }
 
   // setPidGainでゲイン変更後にPID計算が新しいゲインを使用しているかをテスト
-  TEST(PidTest, caluclatePidSetter)
+  TEST(PidTest, CalculatePidAfterSetNewGain)
   {
     Pid actualPid(0.6, 0.05, 0.01, 70, 100.0, -100.0);
     double preCurrentValue = 60;
@@ -132,7 +169,7 @@ namespace etrobocon2025_test {
     double currentValue = 100;
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 10
+     * prevDeviation = 10
      * 2. 現在の誤差
      * currentDeviation = (70 - 100) = -30
      * 3. 誤差の積分を計算
@@ -142,20 +179,20 @@ namespace etrobocon2025_test {
      * 微分項にローパスフィルタを適用
      * filteredDerivative = 0.8 * -4000 + (1 - 0.8) * 800 = -3040
      * 5. PID制御を計算
-     * expected = 0.1 * -30 + 0.2 * -0.1 + 0.3 * -3040 = 915.01
+     * expected = 0.1 * -30 + 0.2 * -0.1 + 0.3 * -3040 = -915.01
      */
     expected = -915.01;
     EXPECT_DOUBLE_EQ(expected, actualPid.calculatePid(currentValue));
   }
 
   // 初回呼び出し時に微分項が正しく計算されるかをテスト
-  TEST(PidTest, derivativeFirstCallEffect)
+  TEST(PidTest, CalculatePidFirstDerivative)
   {
     Pid pid(0.0, 0.0, 1.0, 100, 100.0, -100.0);
-    double currentValu = 90;
+    double currentValue = 90;
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 0
+     * prevDeviation = 0
      * 2. 現在の誤差
      * currentDeviation = (100 - 90) = 10
      * 3. 微分の処理を行う
@@ -166,17 +203,18 @@ namespace etrobocon2025_test {
      * prePID = 0 + 0 + 1.0 * 800 = 800
      */
     double expected = 800.0;
-    EXPECT_DOUBLE_EQ(expected, pid.calculatePid(currentValu));
+    EXPECT_DOUBLE_EQ(expected, pid.calculatePid(currentValue));
   }
 
   // 同じ入力を連続して与えたときに微分出力の絶対値が減少することをテスト
-  TEST(PidTest, culculatePidChangeDerivative)
+  TEST(PidTest, CalculatePidChangeDerivative)
   {
     Pid pid(0.0, 0.0, 1.0, 100, 100.0, -100.0);
-    pid.calculatePid(90);
+    double preCurrentValue = 90;
+    pid.calculatePid(preCurrentValue);
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 0
+     * prevDeviation = 0
      * 2. 現在の誤差
      * currentDeviation = (100 - 90) = 10
      * 3. 微分の処理を行う
@@ -186,10 +224,11 @@ namespace etrobocon2025_test {
      * 5. PID制御を計算
      * prePID = 0 + 0 + 1.0 * 800 = 800
      */
-    double derivationFirst = pid.calculatePid(95);
+    double currentValue = 95;
+    double derivationFirst = pid.calculatePid(currentValue);
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 10
+     * prevDeviation = 10
      * 2. 現在の誤差
      * currentDeviation = (100 - 95) = 5
      * 3. 微分の処理を行う
@@ -199,10 +238,10 @@ namespace etrobocon2025_test {
      * 5. PID制御を計算
      * expected_first = 0 + 0 + 1.0 * -240 = -240
      */
-    double derivationSecond = pid.calculatePid(95);
+    double derivationSecond = pid.calculatePid(currentValue);
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 5
+     * prevDeviation = 5
      * 2. 現在の誤差
      * currentDeviation = (100 - 95) = 5
      * 3. 微分の処理を行う
@@ -220,40 +259,42 @@ namespace etrobocon2025_test {
   }
 
   // 偏差が0の状態が続くと出力も0を維持するかをテスト
-  TEST(PidTest, culculatePidZeroDeviation)
+  TEST(PidTest, CalculatePidZeroDeviation)
   {
     Pid pid(1.0, 1.0, 1.0, 50, 100.0, -100.0);
     double expected = 0.0;
+    double currentValue = 50;
     for(int i = 0; i < 5; ++i) {
-      EXPECT_DOUBLE_EQ(expected, pid.calculatePid(50));
+      EXPECT_DOUBLE_EQ(expected, pid.calculatePid(currentValue));
     }
   }
 
   // 積分項が正しく累積されていくかをテスト
-  TEST(PidTest, clcilatePidIntegralUpdate)
+  TEST(PidTest, CalculatePidIntegralUpdate)
   {
     Pid pid(0.0, 1.0, 0.0, 100, 100.0, -100.0);
-    double first = pid.calculatePid(0);
+    double currentValue = 0;
+    double first = pid.calculatePid(currentValue);
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 0
+     * prevDeviation = 0
      * 2. 現在の誤差
      * currentDeviation = (100 - 0) = 100
      * 3. 誤差の積分を計算
      * integral = 0 + (100 + 0) * 0.01 / 2 = 0.5
      * 4. PID制御を計算
-     * expected_first = 0 + 1.0 * 0.5 + 0 = 0.5
+     * expectedFirst = 0 + 1.0 * 0.5 + 0 = 0.5
      */
-    double second = pid.calculatePid(0);
+    double second = pid.calculatePid(currentValue);
     /** 計算過程
      * 1. 前回の誤差
-     * prevDevation = 100
+     * prevDeviation = 100
      * 2. 現在の誤差
      * currentDeviation = (100 - 100) = 0
      * 3. 誤差の積分を計算
      * integral = 0.5 + (0 + 100) * 0.01 / 2 = 1.5
      * 4. PID制御を計算
-     * expected_first = 0 + 1.0 * 1.5 + 0 = 1.5
+     * expectedSecond = 0 + 1.0 * 1.5 + 0 = 1.5
      */
     double expectedFirst = 0.5;
     double expectedSecond = 1.5;
@@ -262,25 +303,27 @@ namespace etrobocon2025_test {
   }
 
   // 積分項がmaxIntegralで正しく制限されているかをテスト
-  TEST(PidTest, calculatePidIntegralUpperBound)
+  TEST(PidTest, CalculatePidIntegralUpperBound)
   {
     Pid pid(0.0, 1.0, 0.0, 100, 50, -100.0);  // maxIntegral = 50
-    for(int i = 0; i < 500; ++i) {            // 5秒間の累積
-      pid.calculatePid(0);
+    double currentValue = 0;
+    for(int i = 0; i < 500; ++i) {  // 5秒間の累積
+      pid.calculatePid(currentValue);
     }
-    double output = pid.calculatePid(0);
+    double output = pid.calculatePid(currentValue);
     double expected = 50.0;
     EXPECT_DOUBLE_EQ(output, expected);
   }
 
   // 積分項がminIntegralで正しく制限されているかをテスト
-  TEST(PidTest, calculatePidIntegralLowerBound)
+  TEST(PidTest, CalculatePidIntegralLowerBound)
   {
     Pid pid(0.0, 1.0, 0.0, -100, 100.0, -50.0);  // minIntegral = -50
-    for(int i = 0; i < 500; ++i) {               // 5秒間の累積
-      pid.calculatePid(0);
+    double currentValue = 0;
+    for(int i = 0; i < 500; ++i) {  // 5秒間の累積
+      pid.calculatePid(currentValue);
     }
-    double output = pid.calculatePid(0);
+    double output = pid.calculatePid(currentValue);
     double expected = -50.0;
     EXPECT_DOUBLE_EQ(output, expected);
   }
