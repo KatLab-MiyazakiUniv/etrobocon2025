1aa551d0ac2fc2f03f0f60528b79862e2a2e762b
HaruArima08 <uharubxxcd@gmail.com>
2025-05-19 06:38:32 +0900

    add:カラーセンサクラスの実装と値取得のダミーとテストファイル作成

---
+++ tests/dummy/spike/pup/colorsensor.h
@@ -0,0 +1,76 @@
+/**
+ * @file colorSensor.h
+ * @brief カラーセンサクラス (ダミー)
+ * @author HaruArima08
+ */
+
+#ifndef COLOR_SENSOR_H
+#define COLOR_SENSOR_H
+
+#include <stdint.h>
+
+typedef struct {
+  uint16_t r;
+  uint16_t g;
+  uint16_t b;
+} pup_color_rgb_t;
+
+typedef struct {
+  uint16_t h;
+  uint8_t s;
+  uint8_t v;
+} pup_color_hsv_t;
+
+typedef struct {
+} pup_device_t;
+
+// カラーセンサーのデバイス取得関数のダミー (常に同じダミーオブジェクトのポインタを返す)
+inline pup_device_t* pup_color_sensor_get_device(int /*port*/)
+{
+  static pup_device_t dummy;
+  return &dummy;
+}
+
+// カラーセンサーのRGB値取得関数のダミー
+inline pup_color_rgb_t pup_color_sensor_rgb(pup_device_t* /*device*/)
+{
+  return { 32, 36, 40 };
+}
+
+// カラーセンサーの色取得関数のダミー
+inline pup_color_hsv_t pup_color_sensor_color(pup_device_t* /*device*/, bool /*surface*/)
+{
+  return { 0, 83, 111 };
+}
+
+// カラーセンサーの色取得関数のダミー (近似なし)
+inline pup_color_hsv_t pup_color_sensor_hsv(pup_device_t* /*device*/, bool /*surface*/)
+{
+  return { 0, 83, 111 };
+}
+
+// カラーセンサーの反射率取得関数のダミー
+inline int32_t pup_color_sensor_reflection(pup_device_t* /*device*/)
+{
+  return 42;
+}
+
+// カラーセンサーの周囲光強度取得関数のダミー
+inline int32_t pup_color_sensor_ambient(pup_device_t* /*device*/)
+{
+  return 10;
+}
+
+// カラーセンサーのライト設定関数のダミー
+inline void pup_color_sensor_light_set(pup_device_t* /*device*/, int32_t, int32_t, int32_t) {}
+
+// カラーセンサーのライト点灯関数のダミー
+inline void pup_color_sensor_light_on(pup_device_t* /*device*/) {}
+
+// カラーセンサーのライト消灯関数のダミー
+inline void pup_color_sensor_light_off(pup_device_t* /*device*/) {}
+
+// カラーセンサーの検出可能色取得関数のダミー
+inline void pup_color_sensor_detectable_colors(int32_t /*size*/, pup_color_hsv_t* /*colors*/) {}
+
+#endif  // COLOR_SENSOR_H
---
+++ tests/dummy/spikeapi.h
@@ -0,0 +1,10 @@
+/**
+ * @file spikeapi.h
+ * @brief  ヘッダファイルのインクルード(ダミー)
+ * @author HaruArima08
+ */
+
+#ifndef SPIKEAPI_H
+#define SPIKEAPI_H
+
+#endif  // SPIKEAPI_H
