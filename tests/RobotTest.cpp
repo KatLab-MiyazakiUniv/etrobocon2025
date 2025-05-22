cf15fb2f9a1fae92c226f2e69fbc87797905ce6d
takuma <takuchi.1071880@gmail.com>
2025-05-23 01:54:21 +0900

    refactor: RobotTestにnamespaceを指定

--- tests/RobotTest.cpp
+++ tests/RobotTest.cpp
@@ -7,22 +7,26 @@
 #include <gtest/gtest.h>
 #include "Robot.h"
 
-// ゲッターで取得したMotorControllerインスタンスが等しいか確認するテスト
-TEST(RobotTest, GetMotorControllerInstanceReturnsReference)
-{
-  Robot robot;
-  MotorController& motorRef1 = robot.getMotorControllerInstance();
-  MotorController& motorRef2 = robot.getMotorControllerInstance();
+namespace etrobocon2025_test {
 
-  EXPECT_EQ(&motorRef1, &motorRef2);
-}
+  // ゲッターで取得したMotorControllerインスタンスが等しいか確認するテスト
+  TEST(RobotTest, GetMotorControllerInstanceReturnsReference)
+  {
+    Robot robot;
+    MotorController& motorRef1 = robot.getMotorControllerInstance();
+    MotorController& motorRef2 = robot.getMotorControllerInstance();
 
-// ゲッターで取得したCameraCaptureインスタンスが等しいか確認するテスト
-TEST(RobotTest, GetCameraCaptureInstanceReturnsReference)
-{
-  Robot robot;
-  CameraCapture& cameraRef1 = robot.getCameraCaptureInstance();
-  CameraCapture& cameraRef2 = robot.getCameraCaptureInstance();
+    EXPECT_EQ(&motorRef1, &motorRef2);
+  }
 
-  EXPECT_EQ(&cameraRef1, &cameraRef2);
-}
+  // ゲッターで取得したCameraCaptureインスタンスが等しいか確認するテスト
+  TEST(RobotTest, GetCameraCaptureInstanceReturnsReference)
+  {
+    Robot robot;
+    CameraCapture& cameraRef1 = robot.getCameraCaptureInstance();
+    CameraCapture& cameraRef2 = robot.getCameraCaptureInstance();
+
+    EXPECT_EQ(&cameraRef1, &cameraRef2);
+  }
+
+}  // namespace etrobocon2025_test
