e5b973a4ab78a85bb9febd5a1eb4683dc3d86af2
takuma <takuchi.1071880@gmail.com>
2025-05-23 01:16:40 +0900

    add: 動作の親クラスであるMotionクラスを作成

---
+++ modules/motions/Motion.h
@@ -0,0 +1,29 @@
+/**
+ * @file   Motion.h
+ * @brief  動作の親クラス
+ * @author takuchi17
+ */
+
+#ifndef MOTION_H
+#define MOTION_H
+
+#include "Robot.h"
+
+class Motion {
+ public:
+  /**
+   * コンストラクタ
+   * @brief 外部リソースのインスタンスを初期化する
+   */
+  Motion(Robot& _robot);
+
+  /**
+   * @brief 動作を実行する純粋仮想関数
+   */
+  virtual void run() = 0;
+
+ protected:
+  Robot* robot;  // Robotインスタンスのポインタ
+};
+
+#endif
