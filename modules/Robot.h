9ca75a004d8c0874785c2f1e9c8ca7af9edc516e
takuma <takuchi.1071880@gmail.com>
2025-05-22 20:30:46 +0900

    公式APIを直接呼び出せるか，ビルドをCIに任せる

---
+++ modules/Robot.h
@@ -0,0 +1,18 @@
+/**
+ * @file   Robot.h
+ * @brief  外部リソースのインスタンスを管理するクラス
+ * @author takuchi17
+ */
+
+#ifndef ROBOT_H
+#define ROBOT_H
+
+#include "MotorController.h"
+#include "ColorSensor.h"
+
+class Robot {
+ public:
+  Robot();
+
+ private:
+}
