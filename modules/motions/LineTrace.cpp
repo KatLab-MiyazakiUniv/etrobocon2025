bb9b1ead35a59523c2f7cc691d6dc156c74e4ef2
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-05-26 16:07:47 +0900

    add:ライントレースの親クラスLineTrace.cpp,LineTrace.hの追加

---
+++ modules/motions/LineTrace.cpp
@@ -0,0 +1,79 @@
+/**
+ * @file   LineTrace.cpp
+ * @brief  ライントレースの親クラス
+ * @author miyahara046
+ */
+
+#include "LineTrace.h"
+using namespace std;
+
+LineTrace::LineTrace(Robot& _robot, double _targetSpeed, int _targetBrightness,
+                     const PidGain& _pidGain, bool& _isLeftEdge)
+  : colorSensor(EPort::PORT_A),
+    Motion(robot),
+    robot(_robot),
+    targetSpeed(_targetSpeed),
+    targetBrightness(_targetBrightness),
+    pidGain(_pidGain),
+    isLeftEdge(_isLeftEdge)
+
+{
+}
+
+void LineTrace::run()
+{
+  Pid pid(pidGain.kp, pidGain.ki, pidGain.kd, targetBrightness);
+
+  // 初期値を代入
+
+  initDistance = Mileage::calculateMileage(motorController.getRightMotorCount(),
+                                           motorController.getLeftMotorCount());
+
+  // 事前条件を判定する
+  if(!isMetPreCondition(targetSpeed)) {
+    return;
+  }
+
+  // 左右で符号を変える
+  int edgeSign = isLeftEdge ? -1 : 1;
+
+  // 呼び出し時の走行距離
+  initLeftMileage = Mileage::calculateWheelMileage(motorController.getLeftMotorCount());
+  initRightMileage = Mileage::calculateWheelMileage(motorController.getRightMotorCount());
+
+  int logIntervalCount = 0;  // 走行ログを取得するタイミングを計るための変数
+
+  // 継続条件を満たしている間ループ
+  while(isMetContinuationCondition()) {
+    // 初期pwm値を計算
+    double baseRightPwm = motorController.getRightMotorPower();
+    double baseLeftPwm = motorController.getLeftMotorPower();
+
+    // PIDで旋回値を計算
+    double turningPwm = pid.calculatePid(colorSensor.getReflection()) * edgeSign;
+
+    // モータのPWM値をセット（前進の時0を下回らないように，後進の時0を上回らないようにセット）
+    double rightPwm = baseRightPwm > 0.0 ? max(baseRightPwm - turningPwm, 0.0)
+                                         : min(baseRightPwm + turningPwm, 0.0);
+    double leftPwm = baseLeftPwm > 0.0 ? max(baseLeftPwm + turningPwm, 0.0)
+                                       : min(baseLeftPwm - turningPwm, 0.0);
+    motorController.setRightMotorPower(rightPwm);
+    motorController.setLeftMotorPower(leftPwm);
+
+    //  10ループに1回走行ログを取得
+    if(logIntervalCount % 10 == 0) {
+      // 現在の明るさを取得
+      double currentReflection = colorSensor.getReflection();
+
+      // 現在のRGB値を取得
+      spikeapi::ColorSensor::RGB currentRgb;
+      colorSensor.getRGB(currentRgb);
+    }
+    logIntervalCount++;
+
+    // 10ms待機
+    // clock.sleep(10);
+  }
+
+  motorController.stopWheelsMotor();
+}
---
+++ modules/motions/LineTrace.h
@@ -0,0 +1,67 @@
+
+/**
+ * @file   LineTrace.h
+ * @brief  ライントレース
+ * @author miyahara046
+ */
+
+#ifndef LINE_TRACING_H
+#define LINE_TRACING_H
+
+#include "Motion.h"
+#include "Mileage.h"
+#include "Pid.h"
+#include "SystemInfo.h"
+#include "Robot.h"
+// #include "Clock.h"
+
+class LineTrace : public Motion {
+ protected:
+  Robot& robot;  // Robot インスタンスを参照で保持
+
+ public:
+  /**
+   * コンストラクタ
+   * @param _robot Robotインスタンス
+   * @param _targetSpeed 目標速度 0~
+   * @param _targetBrightness 目標輝度 0~
+   * @param _pidGain PIDゲイン
+   * @param _isLeftEdge エッジの左右判定(true:左エッジ, false:右エッジ)
+   */
+  LineTrace(Robot& _robot, double _targetSpeed, int _targetBrightness, const PidGain& _pidGain,
+            bool& _isLeftEdge);
+
+  /**
+   * @brief ライントレースする
+   */
+  void run();
+
+ protected:
+  /**
+   * @brief ライントレースする際の事前条件判定をする
+   * @param targetSpeed 目標速度
+   * @note オーバーライド必須
+   */
+  virtual bool isMetPreCondition(double targetSpeed) = 0;
+
+  /**
+   * @brief ライントレースする際の継続条件判定をする　返り値がfalseでモーターが止まる
+   * @note オーバーライド必須
+   */
+  virtual bool isMetContinuationCondition() = 0;
+
+ protected:
+  double targetSpeed;    // 目標速度 0~
+  int targetBrightness;  // 目標輝度 0~
+  PidGain pidGain;       // PIDゲイン
+
+  bool& isLeftEdge;                 // エッジの左右判定(true:左エッジ, false:右エッジ)
+  double initLeftMileage;           // クラス呼び出し時の左車輪の走行距離
+  double initRightMileage;          // クラス呼び出し時の右車輪の走行距離
+  double initDistance;              // 実行前の走行距離
+  MotorController motorController;  // MotorController
+  spikeapi::ColorSensor colorSensor{ EPort::PORT_A };  // ColorSensor
+  // spikeapi::Clock clock;                               // Clock
+};
+
+#endif
