bb17ddb65a69fc049243380c2b2e570f839fb493
HaruArima08 <uharubxxcd@gmail.com>
2025-05-26 22:15:38 +0900

    add:直進の親クラスの追加,クロッククラスのダミー追加

---
+++ modules/motions/Straight.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file   Straight.cpp
+ * @brief  直進動作の親クラス
+ * @author HaruArima08
+ */
+
+#include "Straight.h"
+using namespace std;
+
+Straight::Straight(Robot& robot, double _targetSpeed)
+  : Motion(robot), targetSpeed(_targetSpeed), initialDistance(0.0)
+{
+}
+void Straight::run()
+{
+  // 事前条件判定が真でないときは終了する
+  if(!isMetPreCondition()) {
+    return;
+  }
+  // 呼び出し時の走行距離を取得する
+  double initialRightMotorCount = robot.getMotorControllerInstance().getRightMotorCount();
+  double initialLeftMotorCount = robot.getMotorControllerInstance().getRightMotorCount();
+  initialDistance = Mileage::calculateMileage(initialRightMotorCount, initialLeftMotorCount);
+
+  // 継続条件を満たしている間繰り返す
+  while(isMetContinuationCondition()) {
+    // Power値を計算
+    double currentLeftPower = robot.getMotorControllerInstance().getRightMotorPower();
+    double currentRightPower = robot.getMotorControllerInstance().getLeftMotorPower();
+
+    // モータにPower値をセット
+    robot.getMotorControllerInstance().setLeftMotorPower(currentLeftPower);
+    robot.getMotorControllerInstance().setRightMotorPower(currentRightPower);
+
+    // 10ミリ秒待機
+    robot.getClockInstance().sleep(10000);
+  }
+
+  // モータを停止
+  robot.getMotorControllerInstance().stopWheelsMotor();
+  robot.getClockInstance().sleep(10000);
+}
---
+++ modules/motions/Straight.h
@@ -0,0 +1,49 @@
+/**
+ * @file   Straight.h
+ * @brief  直進動作の親クラス
+ * @author HaruArima08
+ */
+
+#ifndef STRAIGHT_H
+#define STRAIGHT_H
+
+#include "Motion.h"
+#include "Pid.h"
+#include "Mileage.h"
+
+class Straight : public Motion {
+ public:
+  /**
+   * @brief コンストラクタ
+   * @param robot ロボット本体への参照
+   * @param _speed   目標速度[mm/s]
+   */
+  Straight(Robot& robot, double _speed);
+
+  /**
+   * @brief 直進する
+   */
+  void run();
+
+ protected:
+  /**
+   * @brief 直進する際の事前条件判定をする
+   */
+  virtual bool isMetPreCondition()
+  {
+    return true;  // 事前条件は常に満たす(子クラスで実装)
+  }
+
+  /**
+   * @brief 直進する際の動作継続条件判定をする　返り値がtrueの間モーターが回転
+   * @note オーバーライド必須
+   */
+  virtual bool isMetContinuationCondition() = 0;
+
+ protected:
+  // 目標値は継承後に追加する
+  double targetSpeed;      // 目標速度[mm/s]
+  double initialDistance;  // 初期走行距離
+};
+
+#endif
