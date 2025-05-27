8eae2133cc8dd8c7ab62c6daf85b538acd7d2da9
zima <ibrmark21@gmail.com>
2025-05-27 12:22:12 +0900

    refactor: 細かな修正

--- tests/dummy/Motor.h
+++ tests/dummy/Motor.h
@@ -24,8 +24,8 @@ namespace spikeapi {
     /**
      * コンストラクタ
      * @param port PUPポートID
-     * @param direction モータの回転方向
-     * @param reset_count カウントをリセットするか
+     * @param direction モータの回転方向 初期値はCLOCKWISE
+     * @param reset_count カウントをリセットするか　初期値はtrue
      * pup_motor_setup()を複数回呼ぶとハングするため、コンストラクタで一回だけ呼ぶことでエラーを回避する
      */
     Motor(EPort port, EDirection direction = EDirection::CLOCKWISE, bool reset_count = true)
