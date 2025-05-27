472ff47a7ca3589144cd3e5766a5fa7b2310251e
hara6 <paranalu6@gmail.com>
2025-05-27 14:10:01 +0900

    add: 回頭動作の親クラスの実装。および、それに伴うMakefile.inc、Clockのダミークラスの追加

---
+++ modules/motions/Rotation.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file   Rotation.cpp
+ * @brief  回頭動作の親クラス
+ * @author Hara1274
+ */
+
+#include "Rotation.h"
+
+using namespace std;
+
+Rotation::Rotation(Robot& _robot, int _targetAngle, int _power, bool _isClockwise)
+  : Motion(_robot), targetAngle(_targetAngle), power(_power), isClockwise(_isClockwise)
+{
+}
+
+void Rotation::run()
+{
+  MotorController& motorController = robot.getMotorControllerInstance();
+  // Clock& clock = robot.getClockInstance();
+  if(!isMetPreCondition()) return;
+
+  // 回転方向の符号：時計回り = +1, 反時計回り = -1
+  int leftSign = isClockwise ? 1 : -1;
+  int rightSign = isClockwise ? -1 : 1;
+
+  double initLeftMileage = Mileage::calculateWheelMileage(motorController.getLeftMotorCount());
+  double initRightMileage = Mileage::calculateWheelMileage(motorController.getRightMotorCount());
+
+  double targetDistance = M_PI * TREAD * targetAngle / 360;
+
+  double targetLeftDistance = initLeftMileage + targetDistance * leftSign;
+  double targetRightDistance = initRightMileage + targetDistance * rightSign;
+
+  while(isMetContCondition(targetLeftDistance, targetRightDistance, leftSign, rightSign)) {
+    // パワー値で左右のモーターを制御
+    motorController.setLeftMotorPower(power * leftSign);
+    motorController.setRightMotorPower(power * rightSign);
+
+    clock.sleep(10000);  // 10ms程度のスリープでループを安定させる
+  }
+
+  // モーターを停止
+  motorController.stopWheelsMotor();
+  clock.sleep(10000);
+}
---
+++ modules/motions/Rotation.h
@@ -0,0 +1,60 @@
+/**
+ * @file   Rotation.h
+ * @brief  回頭動作の親クラス
+ * @author Hara1274
+ */
+
+#ifndef ROTATION_H
+#define ROTATION_H
+
+#include "Motion.h"
+#include "Mileage.h"
+#include "SystemInfo.h"
+
+class Rotation : public Motion {
+ public:
+  /**
+   * コンストラクタ
+   * @param _robot       ロボット制御クラスへの参照
+   * @param _targetAngle 目標回転角度(deg) 0~360
+   * @param _power 指定するパワー値（-100 ～ +100）
+   * @param _isClockwise 回頭方向 true:時計回り, false:反時計回り
+   */
+  Rotation(Robot& _robot, int _targetAngle, int power, bool _isClockwise);
+
+ private:
+  /**
+   * @brief 回頭する
+   */
+  void run();
+
+  /**
+   * @brief 回頭する際の事前条件判定をする
+   * @note オーバーライド必須
+   */
+  virtual bool isMetPreCondition() = 0;
+
+  /**
+   * @brief 回頭する際の継続条件判定をする　返り値がfalseでモーターが止まる
+   * @param targetLeftDistance 左車輪の目標距離
+   * @param targetRightDistance 右車輪の目標距離
+   * @param leftSign 左車輪の回転方向
+   * @param rightSign 右車輪の回転方向
+   * @note オーバーライド必須
+   */
+  virtual bool isMetContCondition(double targetLeftDistance, double targetRightDistance,
+                                  int leftSign, int rightSign)
+      = 0;
+
+  /**
+   * @brief 実行のログを取る
+   * @note オーバーライド必須
+   */
+  virtual void logRunning() = 0;
+
+ protected:
+  int targetAngle;   // 目標回転角度(deg) 0~360
+  int power;         // パワー値（-100 ～ +100）
+  bool isClockwise;  // 回頭方向 true:時計回り, false:反時計回り
+};
+#endif
