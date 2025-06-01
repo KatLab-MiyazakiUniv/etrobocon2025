c10df1a1a9f3e2886c7ee72312f6c5a517033b4c
zima <ibrmark21@gmail.com>
2025-06-01 22:46:45 +0900

    add: 円周率の定義とRAD_TO_DEGを追加

--- modules/common/SystemInfo.h
+++ modules/common/SystemInfo.h
@@ -7,6 +7,9 @@
 #ifndef SYSTEM_INFO_H
 #define SYSTEM_INFO_H
 
+#define PI 3.1415926           // 円周率
+#define RAD_TO_DEG 180.0 / PI  // 角度（°）
+
 static constexpr double RADIUS = 28.0;  // 車輪の半径[mm]
 static constexpr double TREAD = 112.0;  // 走行体のトレッド幅（両輪の間の距離）[mm]
 
