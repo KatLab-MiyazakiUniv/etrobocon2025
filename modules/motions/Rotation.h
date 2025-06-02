f88bad73a07e1501519936933077a1c10347dbb7
hara6 <paranalu6@gmail.com>
2025-06-02 20:20:15 +0900

    referctor: コメントの速度の単位を変更

--- modules/motions/Rotation.h
+++ modules/motions/Rotation.h
@@ -15,7 +15,7 @@ class Rotation : public Motion {
    * コンストラクタ
    * @param _robot       ロボット制御クラスへの参照
    * @param _targetAngle 目標回転角度(deg) 0~360
-   * @param _speed       指定する速度値（単位：°/秒）
+   * @param _speed       指定する速度値（mm/秒）
    * @param _isClockwise 回頭方向 true:時計回り, false:反時計回り
    */
   Rotation(Robot& _robot, int _targetAngle, int _speed, bool _isClockwise);
@@ -53,7 +53,7 @@ class Rotation : public Motion {
 
  protected:
   int targetAngle;   // 目標回転角度(deg) 0~360
-  int speed;         // 指定する速度値（単位：°/秒)
+  int speed;         // 指定する速度値（mm/秒）
   bool isClockwise;  // 回頭方向 true:時計回り, false:反時計回り
   int leftSign;      //   leftSign  = +1 のとき左モーターは順回転、 -1 のとき逆回転
   int rightSign;     //   rightSign = +1 のとき右モーターは順回転、 -1 のとき逆回転
