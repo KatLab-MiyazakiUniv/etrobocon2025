56e9c965d6237bc4acd860a0edb99540b6eee5d0
HaruArima08 <uharubxxcd@gmail.com>
2025-06-03 15:49:01 +0900

    add:SpeedCalculatorの追加

---
+++ modules/calculators/SpeedCalculator.cpp
@@ -0,0 +1,47 @@
+/**
+ *  @file SpeedCalculator.cpp
+ *  @brief 目標速度に対応するPWM値を算出するクラス
+ *  @author miyahara046
+ */
+#include "SpeedCalculator.h"
+
+SpeedCalculator::SpeedCalculator(Robot& _robot, double _targetSpeed)
+  : robot(_robot),
+    targetSpeed(_targetSpeed),
+    rightPid(K_P, K_I, K_D, _targetSpeed),
+    leftPid(K_P, K_I, K_D, _targetSpeed)
+{
+  int currentTime = robot.getClockInstance().now();
+  prevRightTime = currentTime;
+  prevLeftTime = currentTime;
+}
+
+double SpeedCalculator::calculateRightMotorPower()
+{
+  // 走行時間を算出
+  int currentRightTime = robot.getClockInstance().now();
+  double diffRightTime = (double)(currentRightTime - prevRightTime);
+  // 右タイヤの走行速度を算出
+  double currentRightSpeed = robot.getMotorControllerInstance().getRightMotorSpeed();
+  // 走行速度に相当する右タイヤのPower値を算出
+  rightMotorPower += rightPid.calculatePid(currentRightSpeed, diffRightTime);
+  // メンバを更新
+  prevRightTime = currentRightTime;
+
+  return rightMotorPower;
+}
+
+double SpeedCalculator::calculateLeftMotorPower()
+{
+  // 走行時間を算出
+  int currentLeftTime = robot.getClockInstance().now();
+  double diffLeftTime = (double)(currentLeftTime - prevLeftTime);
+  // 左タイヤの走行速度を算出
+  double currentLeftSpeed = robot.getMotorControllerInstance().getLeftMotorSpeed();
+  // 走行速度に相当する左タイヤのPower値を算出
+  leftMotorPower += leftPid.calculatePid(currentLeftSpeed, diffLeftTime);
+  // メンバを更新
+  prevLeftTime = currentLeftTime;
+
+  return leftMotorPower;
+}
---
+++ modules/calculators/SpeedCalculator.h
@@ -0,0 +1,49 @@
+/**
+ * @file SpeedCalculator.h
+ * @brief 目標速度に対応するPWM値を算出するクラス
+ * @author miyahara046
+ */
+
+#ifndef SPEED_CALCULATOR_H
+#define SPEED_CALCULATOR_H
+
+#include "Pid.h"
+#include "Robot.h"
+#include "SystemInfo.h"
+
+class SpeedCalculator {
+ public:
+  /**
+   * @brief speedCalculatorのコンストラクタ
+   * @param _robot Robotクラスのインスタンス
+   * @param _targetSpeed 目標とするタイヤ走行速度[mm/s]
+   */
+  SpeedCalculator(Robot& _robot, double _targetSpeed);
+
+  /**
+   * @brief 目標とする走行速度に相当する右車輪のPower値を算出する
+   * @return 走行速度に相当する右タイヤのPower値
+   */
+  double calculateRightMotorPower();
+
+  /**
+   * @brief 目標とする走行速度に相当する左車輪のPower値を算出する
+   * @return 走行速度に相当する左タイヤのPower値
+   */
+  double calculateLeftMotorPower();
+
+ private:
+  const double targetSpeed;
+  Pid rightPid;
+  Pid leftPid;
+  double rightMotorPower;
+  double leftMotorPower;
+  int prevRightTime;
+  int prevLeftTime;
+  Robot& robot;
+  // PIDゲイン
+  static constexpr double K_P = 0.004;
+  static constexpr double K_I = 0.0000005;
+  static constexpr double K_D = 0.0007;
+};
+#endif
