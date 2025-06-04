a0308bf35f0eb6cb6d2da8eb18fadfa4e3d92c1b
zima <ibrmark21@gmail.com>
2025-06-04 13:00:17 +0900

    update: テストの細かい処理の変更

--- tests/DistanceLineTraceTest.cpp
+++ tests/DistanceLineTraceTest.cpp
@@ -8,27 +8,21 @@
 #include <gtest/gtest.h>
 #include <gtest/internal/gtest-port.h>
 
-using namespace std;
+#define ERROR 1.01  // 許容誤差の倍率
 
 namespace etrobocon2025_test {
   // 目標距離までライントレースを行うテストケース（左エッジ走行）
   TEST(DistanceLineTraceTest, RunLeftEdge)
   {
     Robot robot;
-    // Power値の初期化
-    robot.getMotorControllerInstance().resetWheelsMotorPower();
     double targetSpeed = 100.0;
     double targetDistance = 1000.0;
     double targetBrightness = 45.0;
-    double basePower = 100.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
     bool isLeftEdge = true;
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain, isLeftEdge);
 
     double expected = targetDistance;
-
-    // 一回のsetPower()でダミーのモータカウントに加算される値はpower * 0.05
-    double error = Mileage::calculateMileage(basePower * 0.05, basePower * 0.05);  // 許容誤差
 
     dl.run();  // ライントレースを実行
 
@@ -38,27 +32,21 @@ namespace etrobocon2025_test {
     double actual = Mileage::calculateMileage(rightCount, leftCount);
 
     EXPECT_LE(expected, actual);  // ライントレース後に走行した距離が期待する走行距離以上である
-    EXPECT_GT(expected + error, actual);  // ライントレース後に走行した距離が許容誤差未満である
+    EXPECT_GT(expected * ERROR, actual);  // ライントレース後に走行した距離が許容誤差未満である
   }
 
   // 目標距離までライントレースを行うテストケース（右エッジ走行）
   TEST(DistanceLineTraceTest, RunRightEdge)
   {
     Robot robot;
-    // Power値の初期化
-    robot.getMotorControllerInstance().resetWheelsMotorPower();
     double targetSpeed = 100.0;
     double targetDistance = 1000.0;
     double targetBrightness = 45.0;
-    double basePower = 100.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
     bool isLeftEdge = false;
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain, isLeftEdge);
 
     double expected = targetDistance;
-
-    // 一回のsetPower()でダミーのモータカウントに加算される値はpower * 0.05
-    double error = Mileage::calculateMileage(basePower * 0.05, basePower * 0.05);  // 許容誤差
 
     dl.run();  // ライントレースを実行
 
@@ -68,29 +56,22 @@ namespace etrobocon2025_test {
     double actual = Mileage::calculateMileage(rightCount, leftCount);
 
     EXPECT_LE(expected, actual);  // ライントレース後に走行した距離が期待する走行距離以上である
-    EXPECT_GT(expected + error, actual);  // ライントレース後に走行した距離が許容誤差未満である
+    EXPECT_GT(expected * ERROR, actual);  // ライントレース後に走行した距離が許容誤差未満である
   }
 
   // 目標距離までライントレースを行うテストケース（バック，左エッジ走行）
   TEST(DistanceLineTraceTest, RunBackLeftEdge)
   {
     Robot robot;
-    // Power値の初期化
-    robot.getMotorControllerInstance().resetWheelsMotorPower();
     double targetSpeed = -100.0;
     double targetDistance = 100.0;
     double targetBrightness = 45.0;
-    double basePower = -100.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
     bool isLeftEdge = true;
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain, isLeftEdge);
 
     double expected = -targetDistance;
 
-    // 一回のsetPower()でダミーのモータカウントに加算される値はpower * 0.05
-    double error = Mileage::calculateMileage(std::abs(basePower * 0.05),
-                                             std::abs(basePower * 0.05));  // 許容誤差
-
     dl.run();  // ライントレースを実行
 
     // ライントレース後の走行距離
@@ -99,28 +80,21 @@ namespace etrobocon2025_test {
     double actual = Mileage::calculateMileage(rightCount, leftCount);
 
     EXPECT_GE(expected, actual);  // ライントレース後に走行した距離が期待する走行距離以下である
-    EXPECT_LT(expected - error, actual);  // ライントレース後に走行した距離が許容誤差未満である
+    EXPECT_LE(expected * ERROR, actual);  // ライントレース後に走行した距離が許容誤差未満である
   }
 
   // 目標距離までライントレースを行うテストケース（バック，右エッジ走行）
   TEST(DistanceLineTraceTest, RunBackRightEdge)
   {
     Robot robot;
-    // Power値の初期化
-    robot.getMotorControllerInstance().resetWheelsMotorPower();
     double targetSpeed = -100.0;
     double targetDistance = 1000.0;
     double targetBrightness = 45.0;
-    double basePower = -100.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
     bool isLeftEdge = false;
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain, isLeftEdge);
 
     double expected = -targetDistance;
-
-    // 一回のsetPower()でダミーのモータカウントに加算される値はpower * 0.05
-    double error = Mileage::calculateMileage(std::abs(basePower * 0.05),
-                                             std::abs(basePower * 0.05));  // 許容誤差
 
     dl.run();  // ライントレースを実行
 
@@ -130,7 +104,7 @@ namespace etrobocon2025_test {
     double actual = Mileage::calculateMileage(rightCount, leftCount);
 
     EXPECT_GE(expected, actual);  // ライントレース後に走行した距離が期待する走行距離以下である
-    EXPECT_LT(expected - error, actual);  // ライントレース後に走行した距離が許容誤差未満である
+    EXPECT_LT(expected * ERROR, actual);  // ライントレース後に走行した距離が許容誤差未満である
   }
 
   // targetSpeed値が0の時に終了するテストケース
