e52dd35928ac3d0e85fc982e6b1ee838a5024603
takuchi17 <takuchi.1071880@gmail.com>
2025-06-04 23:12:29 +0900

    add: 写真撮影用の動作クラスを作成

---
+++ modules/motions/Snapshot.h
@@ -0,0 +1,31 @@
+/**
+ * @file   Snapshot.h
+ * @brief  写真を撮影して保存するクラス
+ * @author takuchi17
+ */
+
+#ifndef SNAPSHOT_H
+#define SNAPSHOT_H
+
+#include "Motion.h"
+
+class Snapshot : public Motion {
+ public:
+  /**
+   * コンストラクタ
+   * @param _robot 外部リソースのインスタンス
+   *
+   */
+  Snapshot(Robot& _robot, const std::string& _fileName);
+
+  /**
+   * @brief カメラ撮影を行い、画像を保存する
+   */
+  void run() override;
+
+ private:
+  std::string fileName;                                     // 保存するファイル名
+  std::string path = "etrobocon2025/datafiles/snapshots/";  // 保存するパス
+};
+
+#endif
