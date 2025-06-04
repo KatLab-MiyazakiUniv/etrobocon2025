22cc2bc80ead5699eb7f05dd0f7a94c2d4c4682d
takuchi17 <takuchi.1071880@gmail.com>
2025-06-04 23:12:29 +0900

    add: 写真撮影用の動作クラスを作成

---
+++ modules/motions/Snapshot.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file   Snapshot.cpp
+ * @brief  写真を撮影して保存するクラス
+ * @author takuchi17
+ */
+
+#include "Snapshot.h"
+
+Snapshot::Snapshot(Robot& _robot, const std::string& _fileName)
+  : Motion(_robot), fileName(_fileName)
+{
+}
+
+void Snapshot::run()
+{
+  // 写真を撮影する
+  cv::Mat frame;
+  if(!robot.getCameraCaptureInstance().getFrame(frame)) {
+    return;
+  }
+  // 写真を保存する
+  robot.getCameraCaptureInstance().saveFrame(frame, path, fileName);
+}
---
+++ tests/SnapshotTest.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file   SnapshotTest.cpp
+ * @brief  SnapshotTestクラスのテスト
+ * @author takuchi17
+ */
+
+#include <gtest/gtest.h>
+#include "Snapshot.h"
+
+namespace etrobocon2025_test {
+  // CameraCaptureクラスがインスタンス化できるか確認するテスト
+  TEST(SnapshotTest, SnapshotInit)
+  {
+    Robot robot;
+    Snapshot snapshot(robot, "test_snapshot");
+
+    // Snapshotのインスタンスが正常に作成されたか確認
+    EXPECT_NO_THROW(snapshot.run());
+  }
+}  // namespace etrobocon2025_test
