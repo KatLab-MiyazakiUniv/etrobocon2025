b2e241617244f3a0ce07926595cd6cdd0faea8ad
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-06-05 18:05:34 +0900

    add:エッジを変更するクラスの作成

---
+++ modules/motions/EdgeChange.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file   EdgeChange.cpp
+ * @brief  エッジ切り替え
+ * @author miyahara046
+ */
+
+#include "EdgeChange.h"
+
+EdgeChange::EdgeChange(bool& _isLeftEdge, bool _nextEdge)
+  : isLeftEdge(_isLeftEdge), nextEdge(_nextEdge)
+{
+}
+
+void EdgeChange::run()
+{
+  isLeftEdge = nextEdge;
+}
+
+bool EdgeChange::getIsLeftEdge() const
+{
+  return isLeftEdge;
+}
---
+++ modules/motions/EdgeChange.h
@@ -0,0 +1,37 @@
+/**
+ * @file   EdgeChange.h
+ * @brief  エッジ切り替え
+ * @author miyahara046
+ */
+
+#ifndef EDGE_CHANGE
+#define EDGE_CHANGE
+
+#include "Motion.h"
+
+class EdgeChange : public Motion {
+ public:
+  /**
+   * コンストラクタ
+   * @param _isLeftEdge エッジの左右判定(true:左エッジ, false:右エッジ)
+   * @param _nextEdge 切り替え後のエッジ(true:左エッジ, false:右エッジ)
+   */
+  EdgeChange(bool& _isLeftEdge, bool _nextEdge);
+
+  /**
+   * @brief エッジを切り替える
+   */
+  void run();
+
+  /**
+   * @brief 現在のエッジの状態を取得
+   * @return true: 左エッジ, false: 右エッジ
+   */
+  bool getIsLeftEdge() const;
+
+ private:
+  bool& isLeftEdge;  // 参照によって外部の isLeftEdge を操作
+  bool nextEdge;
+};
+
+#endif
