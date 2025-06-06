b0ba14c0296524d8e0a45db5a7a343a443b715e0
takuma <takuchi.1071880@gmail.com>
2025-06-07 04:45:49 +0900

    add: 画像処置用の親クラスを追加

---
+++ modules/image_processors/ImageProcessor.h
@@ -0,0 +1,38 @@
+/**
+ * @file   ImageProcessors.h
+ * @brief  画像処理の親クラス
+ * @author takuchi17
+ */
+
+#ifndef IMAGE_PROCESSOR_H
+#define IMAGE_PROCESSOR_H
+
+#include <string>
+#include <opencv2/opencv.hpp>
+
+struct DetectionResult {
+  bool wasDetected;       // 検出できたかどうか
+  std::string label;      // 検出したラベル
+  cv::Point topLeft;      // 検出したバウンディングボックス領域の左上の座標
+  cv::Point topRight;     // 検出したバウンディングボックス領域の右上の座標
+  cv::Point bottomLeft;   // 検出したバウンディングボックス領域の左下の座標
+  cv::Point bottomRight;  // 検出したバウンディングボックス領域の右下の座標
+};
+
+class ImageProcessor {
+ public:
+  /**
+   * 仮想デストラクタ
+   * @brief 派生クラスのデストラクタが正しく呼ばれるようにするために必要
+   */
+  virtual ~ImageProcessor() = default;
+
+  /**
+   * @brief 画像処理を実行する純粋仮想関数
+   * @param frame 処理対象の画像フレーム
+   * @return 検出結果を含むDetectionResult構造体
+   */
+  virtual DetectionResult process(const cv::Mat& frame) = 0;
+};
+
+#endif
