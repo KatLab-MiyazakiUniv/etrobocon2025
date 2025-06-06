7bed0382eaf8ec2a6f40c245873795c3f4adc509
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-06-06 11:47:28 +0900

    refactor:テストで右エッジ変更を追加

--- tests/DistanceLineTraceTest.cpp
+++ tests/DistanceLineTraceTest.cpp
@@ -20,6 +20,12 @@ namespace etrobocon2025_test {
     double targetDistance = 1000.0;
     double targetBrightness = 45.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
+
+    // 左エッジに変更
+    bool nextEdge = true;
+    EdgeChange ec(robot, nextEdge);
+    ec.run();
+
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain);
 
     double expected = targetDistance;
@@ -43,7 +49,12 @@ namespace etrobocon2025_test {
     double targetDistance = 1000.0;
     double targetBrightness = 45.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
-    bool isLeftEdge = false;
+
+    // エッジを右に変更
+    bool nextEdge = false;
+    EdgeChange ec(robot, nextEdge);
+    ec.run();
+
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain);
 
     double expected = targetDistance;
@@ -68,6 +79,11 @@ namespace etrobocon2025_test {
     double targetBrightness = 45.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
 
+    // 左エッジに変更
+    bool nextEdge = true;
+    EdgeChange ec(robot, nextEdge);
+    ec.run();
+
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain);
 
     double expected = -targetDistance;
@@ -91,7 +107,12 @@ namespace etrobocon2025_test {
     double targetDistance = 1000.0;
     double targetBrightness = 45.0;
     PidGain gain = { 0.1, 0.05, 0.05 };
-    bool isLeftEdge = false;
+
+    // 右エッジに変更
+    bool nextEdge = false;
+    EdgeChange ec(robot, nextEdge);
+    ec.run();
+
     DistanceLineTrace dl(robot, targetDistance, targetSpeed, targetBrightness, gain);
 
     double expected = -targetDistance;
