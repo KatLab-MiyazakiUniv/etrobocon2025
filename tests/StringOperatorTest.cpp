d6c3aa95640d86c067a3d90040df79de8df4301a
hara6 <paranalu6@gmail.com>
2025-06-10 12:16:22 +0900

    refactor: 細かい修正

--- tests/StringOperatorTest.cpp
+++ tests/StringOperatorTest.cpp
@@ -8,13 +8,15 @@
 #include <gtest/gtest.h>
 #include <gtest/internal/gtest-port.h>
 
+using namespace std;
+
 namespace etrobocon2025_test {
 
   // 改行コードを削除するテスト（LF）
   TEST(StringOperatorTest, removeEOL_LF)
   {
-    std::string actual = "StringOperator test\n";
-    std::string expected = "StringOperator test";
+    string actual = "StringOperator test\n";
+    string expected = "StringOperator test";
 
     actual = StringOperator::removeEOL(actual);
 
@@ -24,8 +26,8 @@ namespace etrobocon2025_test {
   // 改行コードを削除するテスト（CR）
   TEST(StringOperatorTest, removeEOL_CR)
   {
-    std::string actual = "StringOperator test\r";
-    std::string expected = "StringOperator test";
+    string actual = "StringOperator test\r";
+    string expected = "StringOperator test";
 
     actual = StringOperator::removeEOL(actual);
 
@@ -35,8 +37,8 @@ namespace etrobocon2025_test {
   // 改行コードを削除するテスト（CRLF）
   TEST(StringOperatorTest, removeEOL_CRLF)
   {
-    std::string actual = "StringOperator test\r\n";
-    std::string expected = "StringOperator test";
+    string actual = "StringOperator test\r\n";
+    string expected = "StringOperator test";
 
     actual = StringOperator::removeEOL(actual);
 
@@ -46,8 +48,8 @@ namespace etrobocon2025_test {
   // 改行のない文字列が渡された場合
   TEST(StringOperatorTest, notRemove)
   {
-    std::string actual = "No newline here";
-    std::string expected = "No newline here";
+    string actual = "No newline here";
+    string expected = "No newline here";
 
     actual = StringOperator::removeEOL(actual);
 
@@ -57,8 +59,8 @@ namespace etrobocon2025_test {
   // 空の文字列が渡された場合
   TEST(StringOperatorTest, emptyString)
   {
-    std::string actual = "";
-    std::string expected = "";
+    string actual = "";
+    string expected = "";
 
     actual = StringOperator::removeEOL(actual);
 
