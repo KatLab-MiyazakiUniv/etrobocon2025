b14050d9d2653be373ac87a8d8196a4ffff5711a
HaruArima08 <uharubxxcd@gmail.com>
2025-06-10 16:14:31 +0900

    add:ダミークラスの追加

---
+++ tests/dummy/Button.h
@@ -0,0 +1,39 @@
+/**
+ * @file Button.h
+ * @brief ボタンクラス(ダミー)
+ * @author HaruArima08 nishijima515
+ */
+
+#ifndef BUTTON_H
+#define BUTTON_H
+
+namespace spikeapi {
+  class Button {
+   public:
+    /**
+     * コンストラクタ
+     */
+    Button(void) = default;
+
+    /**
+     * LEFTボタンが押されているかを確認する
+     */
+    bool isLeftPressed() const { return isPressedFlag; }
+
+    /**
+     * RIGHTボタンが押されているかを確認する
+     */
+    bool isRightPressed() const { return isPressedFlag; }
+
+    /**
+     * インスタンス生成が正常にできたかどうかを確認するための共通メソッド
+     * Buttonでは複数生成が問題ないので、常にfalseを返す
+     */
+    bool hasError();
+
+   private:
+    bool isPressedFlag = true;  // ダミーでは常に押されているとする
+  };
+}  // namespace spikeapi
+
+#endif
