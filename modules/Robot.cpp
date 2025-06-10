6acd52709e06410b1377b8084169b4b9156ab0b7
HaruArima08 <uharubxxcd@gmail.com>
2025-06-10 16:13:13 +0900

    update:必要なAPIの追加

--- modules/Robot.cpp
+++ modules/Robot.cpp
@@ -6,7 +6,16 @@
 
 #include "Robot.h"
 
-Robot::Robot() : motorController(), cameraCapture(), colorSensor(EPort::PORT_E), clock() {}
+Robot::Robot()
+  : motorController(),
+    cameraCapture(),
+    colorSensor(EPort::PORT_E),
+    clock(),
+    button(),
+    forceSensor(EPort::PORT_D),
+    display()
+{
+}
 
 MotorController& Robot::getMotorControllerInstance()
 {
@@ -26,4 +35,29 @@ spikeapi::ColorSensor& Robot::getColorSensorInstance()
 spikeapi::Clock& Robot::getClockInstance()
 {
   return clock;
+}
+
+spikeapi::Button& Robot::getButtonInstance()
+{
+  return button;
+}
+
+spikeapi::ForceSensor& Robot::getForceSensorInstance()
+{
+  return forceSensor;
+}
+
+spikeapi::Display& Robot::getDisplayInstance()
+{
+  return display;
+}
+
+void Robot::setIsLeftEdge(bool isLeft)
+{
+  isLeftEdge = isLeft;  // エッジの左右判定を設定
+}
+
+bool Robot::getIsLeftEdge() const
+{
+  return isLeftEdge;
 }
