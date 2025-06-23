e9aefb837c6aaba30afc210b15bc283a8feab215
HaruArima08 <uharubxxcd@gmail.com>
2025-06-23 12:38:35 +0900

    update:フレーム取得に指定回数以上失敗した場合の処理の追加

--- modules/motions/DistanceCameraLineTrace.h
+++ modules/motions/DistanceCameraLineTrace.h
@@ -1,7 +1,7 @@
 /**
- * @file   DistanceLineTrace.h
- * @brief  指定距離ライントレース動作
- * @author miyahara046
+ * @file   DistanceCameraLineTrace.h
+ * @brief  指定距離カメラライントレース動作
+ * @author miyahara046 HaruArima08
  */
 
 #ifndef DISTANCE_CAMERA_LINE_TRACE_H
@@ -13,44 +13,46 @@ class DistanceCameraLineTrace : public CameraPidTracking {
  public:
   /**
    * コンストラクタ
-   * @param _targetDistance 目標距離 0~
-   * @param _targetSpeed 目標速度 0~
-   * @param _targetBrightness 目標輝度 0~
+   * @param _targetDistance 目標距離
+   * @param _targetSpeed 目標速度
+   * @param _targetPoint 目標x座標
    * @param _pidGain PIDゲイン
+   * @param _boundingBoxDetector 画像処理クラスのポインタ
+   * @param _cameraCapture カメラキャプチャクラスのポインタ
    */
   DistanceCameraLineTrace(Robot& _robot, double _targetDistance, double _targetSpeed,
                           int _targetPoint, const PidGain& _pidGain,
-                          BoundingBoxDetector& _boundingBoxDetector);
+                          BoundingBoxDetector& _boundingBoxDetector, CameraCapture& _cameraCapture);
 
   /**
-   * @brief 指定距離だけライントレースする
+   * @brief 指定距離だけカメラライントレースする
    */
   using CameraPidTracking::run;
 
  protected:
   /**
-   * @brief 指定距離ライントレースする際の事前条件判定をする
+   * @brief 指定距離カメラライントレースする際の事前条件判定をする
    * @param targetSpeed 目標速度
-   * @note オーバーライド必須
    */
 
   bool isMetPreCondition() override;
 
   /**
    * @brief ライントレースする際の事前処理をする
-   * @note オーバーライド必須
    */
   void prepare() override;
 
   /**
-   * @brief 指定距離ライントレースする際の継続条件判定をする　返り値がfalseでモーターが止まる
-   * @note オーバーライド必須
+   * @brief
+   * 指定距離カメラライントレースする際の継続条件判定をする。返り値がfalseでモーターが止まる
    */
   bool isMetContinuationCondition() override;
 
  private:
-  double targetDistance;  // 目標距離 0~
-  double initDistance;    // 実行前の走行距離
+  static constexpr int JUDGE_COUNT = 3;  // フレーム取得最大失敗回数
+  double targetDistance;                 // 目標距離
+  double initDistance;                   // 実行前の走行距離
+  int frameCount;                        // フレーム取得カウント
 };
 
 #endif
---
+++ tests/DistanceCameraLineTraceTest.cpp
@@ -0,0 +1,153 @@
+/**
+ * @file   DistanceCameraLineTraceTest.cpp
+ * @brief  DistanceCameraLineTraceクラスのテスト
+ * @author HaruArima08 miyahara046
+ */
+
+#include <gtest/gtest.h>
+#include "DistanceCameraLineTrace.h"
+#include "DummyBoundingBoxDetector.h"
+#include "DummyCameraCapture.h"
+#include <iostream>
+
+#define ERROR 1.01  // 許容誤差の倍率
+using namespace std;
+
+namespace etrobocon2025_test {
+  // 目標距離までカメラライントレースを行うテストケース
+  TEST(DistanceCameraLineTraceTest, RunDetectCalled)
+  {
+    Robot robot;
+    double targetDistance = 1000.0;
+    double targetSpeed = 500.0;
+    int targetPoint = 320;
+    PidGain gain = { 0.1, 0.05, 0.05 };
+
+    DummyBoundingBoxDetector detector;
+    DummyCameraCapture cameraCapture;
+
+    DistanceCameraLineTrace dcl(robot, targetDistance, targetSpeed, targetPoint, gain, detector,
+                                cameraCapture);
+
+    double expected = targetDistance;
+
+    dcl.run();  // ライントレースを実行
+
+    // カメラライントレース後の走行距離
+    int rightCount = robot.getMotorControllerInstance().getRightMotorCount();
+    int leftCount = robot.getMotorControllerInstance().getLeftMotorCount();
+    double actual = Mileage::calculateMileage(rightCount, leftCount);
+
+    EXPECT_LE(expected, actual);  // ライントレース後に走行した距離が期待する走行距離以上である
+    EXPECT_GT(expected * ERROR, actual);  // ライントレース後に走行した距離が許容誤差未満である
+  }
+
+  // フレーム取得に指定回数以上失敗した場合に終了するテストケース
+  TEST(DistanceCameraLineTraceTest, RunStopFrameFailures)
+  {
+    Robot robot;
+    double targetDistance = 1000.0;
+    double targetSpeed = 500.0;
+    int targetPoint = 320;
+    PidGain gain = { 0.1, 0.05, 0.05 };
+
+    DummyBoundingBoxDetector detector;
+
+    // DummyCameraCaptureで3回連続失敗を設定
+    DummyCameraCapture cameraCapture;
+    cameraCapture.setFrameResults({ true, true, false, false, false });
+
+    DistanceCameraLineTrace dcl(robot, targetDistance, targetSpeed, targetPoint, gain, detector,
+                                cameraCapture);
+
+    dcl.run();  // ライントレースを実行
+
+    // 実際の走行距離を取得
+    int rightCount = robot.getMotorControllerInstance().getRightMotorCount();
+    int leftCount = robot.getMotorControllerInstance().getLeftMotorCount();
+    double actual = Mileage::calculateMileage(rightCount, leftCount);
+
+    // 走行距離が目標距離の20%未満であることを確認
+    EXPECT_LT(actual, targetDistance * 0.2);
+  }
+
+  // targetSpeed値が0の時に終了するテストケース
+  TEST(DistanceCameraLineTraceTest, RunZeroSpeed)
+  {
+    Robot robot;
+    double targetDistance = 1000.0;
+    double targetSpeed = 0.0;
+    int targetPoint = 320;
+    PidGain gain = { 0.1, 0.05, 0.05 };
+
+    DummyBoundingBoxDetector detector;
+    DummyCameraCapture cameraCapture;
+
+    DistanceCameraLineTrace dcl(robot, targetDistance, targetSpeed, targetPoint, gain, detector,
+                                cameraCapture);
+
+    double expected = 0.0;
+    dcl.run();  // ライントレースを実行
+
+    // ライントレース後の走行距離
+    int rightCount = robot.getMotorControllerInstance().getRightMotorCount();
+    int leftCount = robot.getMotorControllerInstance().getLeftMotorCount();
+    double actual = Mileage::calculateMileage(rightCount, leftCount);
+
+    EXPECT_EQ(expected, actual);  // ライントレース前後で走行距離に変化はない
+  }
+
+  // targetDistance値が負の時に終了するテストケース
+  TEST(DistanceCameraLineTraceTest, RunMinusDistance)
+  {
+    Robot robot;
+    double targetDistance = -1000.0;
+    double targetSpeed = 500.0;
+    int targetPoint = 320;
+    PidGain gain = { 0.1, 0.05, 0.05 };
+
+    DummyBoundingBoxDetector detector;
+    DummyCameraCapture cameraCapture;
+
+    DistanceCameraLineTrace dcl(robot, targetDistance, targetSpeed, targetPoint, gain, detector,
+                                cameraCapture);
+
+    double expected = 0.0;
+
+    dcl.run();  // ライントレースを実行
+
+    // ライントレース後の走行距離
+    int rightCount = robot.getMotorControllerInstance().getRightMotorCount();
+    int leftCount = robot.getMotorControllerInstance().getLeftMotorCount();
+    double actual = Mileage::calculateMileage(rightCount, leftCount);
+
+    EXPECT_EQ(expected, actual);  // ライントレース前後で走行距離に変化はない
+  }
+
+  // targetDistance値が0のとき終了するテストケース
+  TEST(DistanceCameraLineTraceTest, RunZeroDistance)
+  {
+    Robot robot;
+    double targetDistance = 0.0;
+    double targetSpeed = 500.0;
+    int targetPoint = 320;
+    PidGain gain = { 0.1, 0.05, 0.05 };
+
+    DummyBoundingBoxDetector detector;
+    DummyCameraCapture cameraCapture;
+
+    DistanceCameraLineTrace dcl(robot, targetDistance, targetSpeed, targetPoint, gain, detector,
+                                cameraCapture);
+
+    double expected = 0.0;
+
+    dcl.run();  // ライントレースを実行
+
+    // ライントレース後の走行距離
+    int rightCount = robot.getMotorControllerInstance().getRightMotorCount();
+    int leftCount = robot.getMotorControllerInstance().getLeftMotorCount();
+    double actual = Mileage::calculateMileage(rightCount, leftCount);
+
+    EXPECT_EQ(expected, actual);  // ライントレース前後で走行距離に変化はない
+  }
+}  // namespace etrobocon2025_test
