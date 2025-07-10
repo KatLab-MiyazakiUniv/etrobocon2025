484fdb04d7377a681c3ea403a6fa6696b31641e7
HaruArima08 <uharubxxcd@gmail.com>
2025-07-10 19:56:41 +0900

    refactor:formatチェックの適用

--- modules/motions/Straight.cpp
+++ modules/motions/Straight.cpp
@@ -32,7 +32,7 @@ void Straight::run()
     robot.getMotorControllerInstance().setRightMotorPower(currentRightPower);
     robot.getMotorControllerInstance().setLeftMotorPower(currentLeftPower);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(10)); // 10000マイクロ秒(10ミリ秒)待機
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));  // 10000マイクロ秒(10ミリ秒)待機
   }
 
   // モータを停止
