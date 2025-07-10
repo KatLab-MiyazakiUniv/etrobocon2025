804d39da7e7b40a9d091cc994176ea78c41b00ab
HaruArima08 <uharubxxcd@gmail.com>
2025-07-10 19:55:37 +0900

    update:ライントレース調整

--- modules/EtRobocon2025.cpp
+++ modules/EtRobocon2025.cpp
@@ -1,22 +1,21 @@
 #include "EtRobocon2025.h"
 #include "AreaMaster.h"
-#include <iostream>
 
 Robot EtRobocon2025::robot;  // Robotインスタンス
 
 void EtRobocon2025::start()
 {
   std::cout << "Hello KATLAB" << std::endl;
-  
+
   robot.getCameraCaptureInstance().setCameraID(
       robot.getCameraCaptureInstance().findAvailableCameraID());
   robot.getCameraCaptureInstance().openCamera();
 
   cv::Mat frame;
-  while(!robot.getCameraCaptureInstance().getFrame(frame)){ 
+  while(!robot.getCameraCaptureInstance().getFrame(frame)) {
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }    
-  
+  }
+
   Calibrator calibrator(robot);
   calibrator.selectAndSetCourse();
   calibrator.measureAndSetTargetBrightness();
