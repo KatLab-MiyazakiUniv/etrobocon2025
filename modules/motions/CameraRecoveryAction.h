e25af52f23ab86be332feeb4501609049716cfd8
HaruArima08 <uharubxxcd@gmail.com>
2025-08-03 16:12:34 +0900

    update:不要なメンバ関数と細かい修正

--- modules/motions/CameraRecoveryAction.h
+++ modules/motions/CameraRecoveryAction.h
@@ -7,16 +7,16 @@
 #ifndef CAMERA_RECOVERY_ACTION_H
 #define CAMERA_RECOVERY_ACTION_H
 
-#include "Motion.h"
+#include "CompositeMotion.h"
 #include "BoundingBoxDetector.h"
 #include "AngleRotation.h"
 
-class CameraRecoveryAction : public Motion {
+class CameraRecoveryAction : public CompositeMotion {
  public:
   /**
    * コンストラクタ
    * @param _robot ロボットインスタンス
-   * @param _angle 回頭角度 (dig) 0~360
+   * @param _angle 回頭角度 (dig) 0~180
    * @param _speed 回頭速度（mm/秒）
    * @param _isClockwise 回頭方向（true: 右回り, false: 左回り）
    * @param _boundingBoxDetector 画像処理クラスのポインタ
@@ -29,12 +29,6 @@ class CameraRecoveryAction : public Motion {
    */
   void run() override;
 
-  /**
-   * @brief 復帰動作が成功したかチェック
-   * @return true: 復帰成功, false: 復帰失敗
-   */
-  bool isRecoverySuccessful() const;
-
  private:
   std::unique_ptr<BoundingBoxDetector> boundingBoxDetector;  // 画像処理クラスのポインタ
   BoundingBoxDetectionResult result;                         // 検出結果
@@ -42,7 +36,6 @@ class CameraRecoveryAction : public Motion {
   double speed;                                              // 回頭スピード
   bool isClockwise;                                          // 回頭方向
   static constexpr int FRAME_NUMBER = 5;                     // フレーム取得回数
-  bool recoverySuccess = false;                              // 復帰成功フラグ
 };
 
 #endif
