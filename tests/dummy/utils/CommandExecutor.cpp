dc572249cb79a74b58f4583ab80002ff1dd6fb72
takuchi17 <takuchi.1071880@gmail.com>
2025-08-03 22:08:30 +0900

    fix: テスト用CommandExecutorの関数定義の仕方を修正

--- tests/dummy/utils/CommandExecutor.cpp
+++ tests/dummy/utils/CommandExecutor.cpp
@@ -4,14 +4,12 @@
  * @author takuchi17
  */
 
+#include "CommandExecutor.h"
 #include <iostream>
 #include <string>
 
-class CommandExecutor {
- public:
-  static int exec(const std::string& command)
-  {
-    std::cout << "[Dummy CommandExecutor] Simulated exec: " << command << std::endl;
-    return 0;
-  }
-};
+int CommandExecutor::exec(const std::string& command)
+{
+  std::cout << "[Dummy CommandExecutor] Simulated exec: " << command << std::endl;
+  return 0;
+}
