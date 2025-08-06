85b471fe4930a9a8b80cd66daddf8166a023d314
hara6 <paranalu6@gmail.com>
2025-08-07 01:32:04 +0900

    refactor: MiniFigCameraActionで名前空間を使うように

--- modules/motions/MiniFigCameraAction.cpp
+++ modules/motions/MiniFigCameraAction.cpp
@@ -30,7 +30,7 @@ bool MiniFigCameraAction::isMetPreCondition()
 {
   if(position != 0 && robot.getMiniFigDirectionResult().wasDetected
      && robot.getMiniFigDirectionResult().direction != static_cast<MiniFigDirection>(position)) {
-    std::cout << "ミニフィグの撮影動作は行わない。" << std::endl;
+    cout << "ミニフィグの撮影動作は行わない。" << endl;
     return false;
   } else {
     return true;
@@ -49,20 +49,20 @@ void MiniFigCameraAction::detectDirection(cv::Mat& frame)
 
   // デバッグ出力
   if(!result.wasDetected) {
-    std::cout << "ミニフィグが検出されませんでした" << std::endl;
+    cout << "ミニフィグが検出されませんでした" << endl;
   }
   switch(result.direction) {
     case MiniFigDirection::FRONT:
-      std::cout << "ミニフィグの向き: FRONT" << std::endl;
+      cout << "ミニフィグの向き: FRONT" << endl;
       break;
     case MiniFigDirection::BACK:
-      std::cout << "ミニフィグの向き: BACK" << std::endl;
+      cout << "ミニフィグの向き: BACK" << endl;
       break;
     case MiniFigDirection::LEFT:
-      std::cout << "ミニフィグの向き: LEFT" << std::endl;
+      cout << "ミニフィグの向き: LEFT" << endl;
       break;
     case MiniFigDirection::RIGHT:
-      std::cout << "ミニフィグの向き: RIGHT" << std::endl;
+      cout << "ミニフィグの向き: RIGHT" << endl;
       break;
   }
 }
@@ -79,7 +79,7 @@ void MiniFigCameraAction::run()
   preAR.run();
 
   // 動作安定のためのスリープ
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  this_thread::sleep_for(chrono::milliseconds(10));
 
   // 後退
   DistanceStraight back(robot, backTargetDistance, -backSpeed);
@@ -93,38 +93,38 @@ void MiniFigCameraAction::run()
 
   if(position == 0) {
     // 向きの判定とresultの更新(detection)は一回目(初期位置での)の撮影でしか行わない
-    std::cout << "判定動作実施" << std::endl;
+    cout << "判定動作実施" << endl;
     detectDirection(frame);
-    std::cout << "判定動作終了" << std::endl;
+    cout << "判定動作終了" << endl;
 
     if(robot.getMiniFigDirectionResult().wasDetected
        && robot.getMiniFigDirectionResult().direction == MiniFigDirection::FRONT) {
       // FRONT方向の画像を保存
       FrameSave::save(frame, filePath, uploadFileName);
       // 非同期で画像をアップロード
-      std::thread([this] { ImageUploader::uploadImage(filePath, uploadFileName, 3); }).detach();
+      thread([this] { ImageUploader::uploadImage(filePath, uploadFileName, 3); }).detach();
     }
 
   } else if(robot.getMiniFigDirectionResult().wasDetected) {
     // 一回目の撮影でミニフィグが検出されていて、向きがFRONTじゃなければ、二回目の撮影でのミニフィグの向きは確実にFRONTになる。
-    std::cout << "正面での撮影" << std::endl;
+    cout << "正面での撮影" << endl;
     FrameSave::save(frame, filePath, uploadFileName);
     // 非同期で画像をアップロード
-    std::thread([this] { ImageUploader::uploadImage(filePath, uploadFileName, 3); }).detach();
+    thread([this] { ImageUploader::uploadImage(filePath, uploadFileName, 3); }).detach();
   } else {
     // 一回目検出falseなら、残り、3回の撮影は確定する。
     // 一回目の撮影でミニフィグが検出されていない場合は、残り3つのすべてのpositionで撮影を行い、画像をpositionごとに保存する。
-    std::cout << "ミニフィグ向き判定用写真の撮影" << std::endl;
-    std::string positionImageName = "Fig_" + to_string(position);
+    cout << "ミニフィグ向き判定用写真の撮影" << endl;
+    string positionImageName = "Fig_" + to_string(position);
     FrameSave::save(frame, filePath, positionImageName);
     // 非同期で画像をアップロード
-    std::thread([this, positionImageName] {
+    thread([this, positionImageName] {
       ImageUploader::uploadImage(filePath, positionImageName, 3);
     }).detach();
   }
 
   // 動作安定のためのスリープ
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  this_thread::sleep_for(chrono::milliseconds(10));
 
   robot.getMotorControllerInstance().setLeftMotorPower(0);
   robot.getMotorControllerInstance().setRightMotorPower(0);
@@ -133,13 +133,13 @@ void MiniFigCameraAction::run()
   forward.run();
 
   // 動作安定のためのスリープ
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  this_thread::sleep_for(chrono::milliseconds(10));
 
   robot.getMotorControllerInstance().setLeftMotorPower(0);
   robot.getMotorControllerInstance().setRightMotorPower(0);
 
   // 動作安定のためのスリープ
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  this_thread::sleep_for(chrono::milliseconds(10));
 
   // 黒線復帰のための回頭をする
   AngleRotation postAR(robot, postTargetAngle, targetRotationSpeed, !isClockwise);
