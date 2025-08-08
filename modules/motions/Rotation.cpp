c2b6534ef6e758173cc7cc22bfe71ad28a20a297
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-08-09 02:04:50 +0900

    setSpeedを使用

--- modules/motions/Rotation.cpp
+++ modules/motions/Rotation.cpp
@@ -31,25 +31,11 @@ void Rotation::run()
   const double targetLinearVelocity = targetAngularVelocityRad * halfTread;
 
   // SpeedCalculator は正の速度のみ受け取る想定なので絶対値で渡す
-  SpeedCalculator speedCalculator(robot, std::fabs(targetLinearVelocity));
+  spikeapi::MotorController& motorController = robot.getMotorControllerInstance();
+  motorController.setRightMotorSpeed(targetLinearVelocity * rightSign);
+  motorController.setLeftMotorSpeed(targetLinearVelocity * leftSign);
 
   while (isMetContinuationCondition()) {
-    // 左右のベースパワーを取得
-    double baseRightPower = speedCalculator.calculateRightMotorPower();
-    double baseLeftPower = speedCalculator.calculateLeftMotorPower();
-
-    // 回転方向に合わせて符号をつける
-    double turningSign = (leftSign > 0) ? 1.0 : -1.0;
-
-    // 左右に回転パワー差を付ける（旋回分）
-    double rightPower = baseRightPower * (-turningSign);
-    double leftPower = baseLeftPower * turningSign;
-
-    // モーターにセット
-    robot.getMotorControllerInstance().setRightMotorPower(rightPower);
-    robot.getMotorControllerInstance().setLeftMotorPower(leftPower);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 
   robot.getMotorControllerInstance().brakeWheelsMotor();
