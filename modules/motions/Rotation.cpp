4bfb00dd6239bccceebdb65007b9fda84cc40b91
hara6 <paranalu6@gmail.com>
2025-08-17 22:44:13 +0900

    update: Rotationの親クラスの変更

--- modules/motions/Rotation.cpp
+++ modules/motions/Rotation.cpp
@@ -6,9 +6,8 @@
 
 #include "Rotation.h"
 
-Rotation::Rotation(Robot& _robot, double _speed, bool _isClockwise)
+Rotation::Rotation(Robot& _robot, bool _isClockwise)
   : Motion(_robot),
-    speed(_speed),
     isClockwise(_isClockwise),
     leftSign(_isClockwise ? 1 : -1),
     rightSign(_isClockwise ? -1 : 1)
@@ -23,14 +22,14 @@ void Rotation::run()
   prepare();
   if(!isMetPreCondition()) return;
 
-  // 回転速度（mm/秒）で指定しモーターを制御
-  motorController.setLeftMotorSpeed(speed * leftSign);
-  motorController.setRightMotorSpeed(speed * rightSign);
-
   while(isMetContinuationCondition()) {
-    // 回頭継続条件が満たされるまで待機（モーターが走行中）
+    // 動的モーター制御
+    updateMotorControl();
   }
 
   // モーターを停止
   motorController.stopWheelsMotor();
+  motorController.setLeftMotorPower(0);
+  motorController.setRightMotorPower(0);
+
 }
