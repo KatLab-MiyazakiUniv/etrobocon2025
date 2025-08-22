9c2077d8e6f5c17aefa60ae9e90c73666df9f1fb
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-08-22 14:33:31 +0900

    add: UltrasonicDistanceCameraLineTraceの追加

--- modules/EtRobocon2025.cpp
+++ modules/EtRobocon2025.cpp
@@ -12,5 +12,34 @@ Robot EtRobocon2025::robot;  // Robotインスタンス
 void EtRobocon2025::start()
 {
   std::cout << "Hello KATLAB" << std::endl;
-  std::cout << "distance =" << robot.getUltraSonicInstance().getDistance() << std::endl;
+
+  if(!robot.getCameraCaptureInstance().setCameraID(
+         robot.getCameraCaptureInstance().findAvailableCameraID()))
+    return;
+  if(!robot.getCameraCaptureInstance().openCamera()) return;
+
+  cv::Mat frame;
+  while(!robot.getCameraCaptureInstance().getFrame(frame)) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+
+  Calibrator calibrator(robot);
+  // calibrator.selectAndSetCourse();
+  // calibrator.measureAndSetTargetBrightness();
+  bool isLeftCourse = true;
+  int targetBrightness = 51;
+  calibrator.getAngleCheckFrame();
+  calibrator.waitForStart();
+
+  // Area lineTraceArea = Area::LineTrace;
+  // AreaMaster lineTraceAreaMaster(robot, lineTraceArea, isLeftCourse, targetBrightness);
+  // lineTraceAreaMaster.run();
+
+  // Area lineTraceArea = Area::LineTrace;
+  // AreaMaster lineTraceAreaMaster(robot, lineTraceArea, isLeftCourse, targetBrightness);
+  // lineTraceAreaMaster.run();
+
+  Area SmartCarryArea = Area::SmartCarry;
+  AreaMaster smartCarryAreaMaster(robot, SmartCarryArea, isLeftCourse, targetBrightness);
+  smartCarryAreaMaster.run();
 }
