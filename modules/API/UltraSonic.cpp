9cc1634b6bf0a1441ba4f0a80480817dd740bb07
zima <ibrmark21@gmail.com>
2025-08-29 10:39:07 +0900

    add: 超音波センサーのラッパークラスの追加

---
+++ modules/API/UltraSonic.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file UltraSonic.cpp
+ * @brief 超音波センサー制御に用いる関数をまとめたラッパークラス
+ * @author nishijima515
+ */
+
+#include "UltraSonic.h"
+
+using namespace spikeapi;
+
+// staticメンバの定義＆初期化（ここで1回だけ）
+spikeapi::UltrasonicSensor UltraSonic::ultraSonic(EPort::PORT_F);
+
+UltraSonic::UltraSonic() {}
+
+double UltraSonic::getDistance()
+{
+  // 超音波センサーの距離を取得
+  return static_cast<double>(ultraSonic.getDistance());
+}
+
+// void UltraSonic::init() {
+//   // 超音波センサーの初期化
+//   UltrasonicSensor sensor(EPort::PORT_1); // 適切なポートを指定
+//   sensor.init();
+// }
