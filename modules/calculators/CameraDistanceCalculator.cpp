8e59b4a267120bf2593f581980622d66cc7d2938
zima <ibrmark21@gmail.com>
2025-11-04 12:59:35 +0900

    add: カメラ画像距離算出クラスの実装

---
+++ modules/calculators/CameraDistanceCalculator.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file   CameraDistanceCalculator.cpp
+ * @brief  カメラ画像を使って、走行体から対象物までの距離を計算するクラス
+ * @author nishijima515
+ */
+
+#include "CameraDistanceCalculator.h"
+
+CameraDistanceCalculator::CameraDistanceCalculator(Robot& _robot) : robot(_robot) {}
+
+double CameraDistanceCalculator::calculateDistance(
+    const CameraServer::BoundingBoxDetectorResponse& response)
+{
+  // バウンディングボックスの中心座標を取得
+  double currentX = (response.result.topLeft.x + response.result.bottomRight.x) / 2.0;
+  double currentY = (response.result.topLeft.y + response.result.bottomRight.y) / 2.0;
+
+  // 対象物から画角の一番下までのピクセル数を計算
+  double bottomToCurrentY = BOTTOM_Y - currentY;
+
+  // コース上での実際の長さに変換(スマートキャリーのランディング用)
+
+  double onePixelDistance
+      = BLUECIRCLE_REAL_WIDTH / (response.result.bottomRight.x - response.result.topLeft.x);
+  double distance = (bottomToCurrentY * onePixelDistance) + BOTTOM_TO_ROBOT;
+
+  return distance;
+}
