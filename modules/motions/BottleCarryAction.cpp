fd78c92f0c2c65470038873a60c4c107c8d21430
miyahara046 <taka.rocket.nba.1104@gmail.com>
2025-11-06 11:41:36 +0900

    update: 補正回頭の処理をコメントアウト

--- modules/motions/BottleCarryAction.cpp
+++ modules/motions/BottleCarryAction.cpp
@@ -56,23 +56,23 @@ void BottleCarryAction::run()
     ss.run();
   }
 
-  // 青丸に直進するための補正角度計算
-  while(1) {
-    GetCorrectionAngle correction(robot);
-    GetCorrectionAngleResult correctionResult;
-    correctionResult = correction.GetCorrectAngle(targetXCoordinate, detectionRequest);
-    std::cout << "補正回頭角度：" << correctionResult.correctionAngle << std::endl;
-    IMUAngleRotation rotate(robot, correctionResult.correctionAngle, 60.0,
-                            correctionResult.isClockwise, PidGain(0.036, 0.012, 0.03), false);
-    if(correctionResult.correctionAngle <= 3.0) {
-      rotate.run();
-      break;
-    }
-    rotate.run();
-  }
+  // // 青丸に直進するための補正角度計算
+  // while(1) {
+  //   GetCorrectionAngle correction(robot);
+  //   GetCorrectionAngleResult correctionResult;
+  //   correctionResult = correction.GetCorrectAngle(targetXCoordinate, detectionRequest);
+  //   std::cout << "補正回頭角度：" << correctionResult.correctionAngle << std::endl;
+  //   IMUAngleRotation rotate(robot, correctionResult.correctionAngle, 60.0,
+  //                           correctionResult.isClockwise, PidGain(0.036, 0.012, 0.03), false);
+  //   if(correctionResult.correctionAngle <= 3.0) {
+  //     rotate.run();
+  //     break;
+  //   }
+  //   rotate.run();
+  // }
 
-  // 動作安定のためのスリープ
-  this_thread::sleep_for(chrono::milliseconds(10));
+  // // 動作安定のためのスリープ
+  // this_thread::sleep_for(chrono::milliseconds(10));
 
   // 青丸までの距離をカメラで計測
   CameraDistanceCalculator calculator(robot);
